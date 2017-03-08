95e471783b443f6b2df64a8ee0dbb437ff391517
SpeakingCraniums <wesley.weissenberger@gmail.com>
2017-03-08 15:32:28 -0500

    March 8th.  Steam VR integration. Re work attraction
    Using Sebastian Lague's code for attractors.   Camera rig is in Scene
    and working.

--- threeDRts/Assets/Scripts/Planets/Planet.cs
+++ threeDRts/Assets/Scripts/Planets/Planet.cs
@@ -5,14 +5,8 @@ using UnityEngine;
 public class Planet : MonoBehaviour {
 	//will need spin and stuff
 
-	public Vector3 gravHome;
-	private SphereCollider planetCollider;
-	public float radiusOfPlanet;
-
 	void Start () {
-		gravHome = transform.position;
-		planetCollider = GetComponent<SphereCollider>();
-		radiusOfPlanet = planetCollider.radius;
+
 	}
 	
 	// Update is called once per frame
